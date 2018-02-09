8cc4ef4e89e78d9111ac219e714f3f5a4ebdd3e8
Ken Grierson <ken.grierson@cybg.com>
2018-02-09 12:14:39 +0000

    DYB-000 Add pom

--- src/main/java/com/unimeron/services/play/Application.java
+++ src/main/java/com/unimeron/services/play/Application.java
@@ -3,24 +3,13 @@ package com.unimeron.services.play;
 import com.unimeron.services.play.configuration.JerseyConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder
-                        .sources(Application.class)
-                        .bannerMode(Banner.Mode.OFF)
-                        ;
-    }
+public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
